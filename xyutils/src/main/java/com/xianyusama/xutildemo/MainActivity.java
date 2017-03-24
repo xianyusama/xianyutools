46d1e14b163e15f4f6814e15d82f18f48d47cf47
xianyu <xianyusama@gmail.com>
2017-03-25 07:56:05 +0800

    删除dialog

--- xyutils/src/main/java/com/xianyusama/xutildemo/MainActivity.java
+++ xyutils/src/main/java/com/xianyusama/xutildemo/MainActivity.java
@@ -3,45 +3,20 @@ package com.xianyusama.xutildemo;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import com.xianyusama.xutildemo.widget.simpledialog.SweetAlertDialog;
 
 
 public class MainActivity extends AppCompatActivity {
 
-    private SweetAlertDialog sweetAlertDialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        sweetAlertDialog = new SweetAlertDialog(MainActivity.this);
-        sweetAlertDialog.setTitleText("hahahahah");
-        sweetAlertDialog.show();
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    Thread.sleep(5000);
-                    runOnUiThread(new Runnable() {
-                        @Override
-                        public void run() {
-                            if (sweetAlertDialog != null) {
-                                sweetAlertDialog.cancel();
-                            }
-                        }
-                    });
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }).start();
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        if (sweetAlertDialog != null) {
-            sweetAlertDialog.cancel();
-        }
+
     }
 }
